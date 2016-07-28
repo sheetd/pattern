a073f7f4b9c325f9a3c6b317f5d45075282748ac
Neil Meredith <neil@sheetd.com>
2016-07-27 22:41:37 -0400

    added canvas grid elements

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
+$(document).ready(function() {
+    drawCanvas();
+});
+
 // Update sliders with # values
 function outputUpdate(int) {
 	document.querySelector("#cellCountDisplay").value = int;
+}
+
+function drawCanvas() {
+	// Draw grid lines
+	var c = document.getElementById("myCanvas");
+	var context = c.getContext("2d");
+
+	for (var x = 0.5; x < 501; x += 10) {
+	  context.moveTo(x, 0);
+	  context.lineTo(x, 381);
+	}
+
+	for (var y = 0.5; y < 381; y += 10) {
+	  context.moveTo(0, y);
+	  context.lineTo(500, y);
+	}
+
+	context.strokeStyle = "#ddd";
+	context.stroke();
 }
